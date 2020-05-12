c852253c1469e33259da7c0796cce01f8df4b62f
arybayunurwicaksono <61864279+arybayunurwicaksono@users.noreply.github.com>
2020-05-12 14:57:56 +0700

    Tugas Lab 6 - OOP 4

---
+++ polymorphism.inheritance/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismInterface
+{
+    class PrinterWindows : IPrinterWindows
+    {
+        public void Show()
+        {
+            Console.WriteLine("Printer Windows display dimension: ");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Printer Windows printing...");
+        }
+    }
+
+    interface IPrinterWindows
+    {
+        void Show();
+        void Print();
+    }
+
+    class Canon : IPrinterWindows
+    {
+        public void Show()
+        {
+            Console.WriteLine("Printer Canon dimension: 9.5 * 12");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Canon printer printing ....");
+        }
+    }
+
+    class Epson : IPrinterWindows
+    {
+        public void Show()
+        {
+            Console.WriteLine("Printer Epson dimension: 10 * 11");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Epson printer printing ....");
+        }
+    }
+
+    class LaserJet : IPrinterWindows
+    {
+        public void Show()
+        {
+            Console.WriteLine("Printer LaserJet dimension: 12 * 12");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("LaserJet printer printing ....");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pilih Printer: \n1. Epson\n2. Canon\n3. LaserJet");
+
+            Console.Write("\nNomor Printer [1..3] : ");
+            int nomorPrinter = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            IPrinterWindows printer = new PrinterWindows();
+
+            if (nomorPrinter == 1)
+                printer = new Epson();
+            else if (nomorPrinter == 2)
+                printer = new Canon();
+            else
+                printer = new LaserJet();
+
+            printer.Show();
+            printer.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
